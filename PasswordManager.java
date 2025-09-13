48d0e3f508e7d75345b67009e4f89c8c688ddcb0
Om <shelkeom67@gmail.com>
2025-09-13 19:10:11 +0530

    Write The code for taking master password as input

---
+++ PasswordManager.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class PasswordManager {
+    public static void main(String args[]) throws Exception {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Set/Enter Master Password:");
+
+        String master=sc.nextLine();
+    }
+    
+}
