e57e2bed6b869d16cff35d44fe4c04eaf4215c61
Om <shelkeom67@gmail.com>
2025-09-15 23:16:58 +0530

    For generating AES key from master password

--- PasswordManager.java
+++ PasswordManager.java
@@ -1,11 +1,21 @@
 import java.util.*;
 
 public class PasswordManager {
+
+    private static void setMasterPassWord(String masterPassWord) throws Exception {
+        byte[] keybytes=Array.copyOf(masterPassWord.getBytes(),16);
+
+        secretKey=new SecretKeySpec(keyBytes,ALGORITHM);
+    }
+
+
+
     public static void main(String args[]) throws Exception {
         Scanner sc=new Scanner(System.in);
         System.out.print("Set/Enter Master Password:");
 
         String master=sc.nextLine();
+        setMasterPassWord(master);
     }
     
 }
